d9508baacf874cec89ebc668b522f738e83fe1b4
Bryce512 <bryce.whitworth@gmail.com>
2024-02-21 10:27:28 -0700

    sync

---
+++ Premier Automation/C++ automation/classCodesMap.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <map>
+#include <string>
+
+std::map<std::string, std::string> classMap = {
+    {"180", "dance-180-american-cha-cha"},
+    {"181", "dance-181-country-western-two-step"},
+    {"184", "dance-184-ballroom-quickstep"},
+    {"185", "dance-185-latin-samba"},
+    {"280", "dance-280-american-swing"},
+    {"281", "dance-281-country-western-polka"},
+    {"284", "dance-284-ballroom-waltz"},
+    {"285", "dance-285-latin-rumba"},
+    {"380", "dance-380-american-foxtrot"},
+    {"382", "dance-382-ballroom-tango"},
+    {"383", "dance-383-latin-paso-doble"},
+    {"384", "dance-384-ballroom-waltz"},
+    {"385", "dance-385-latin-samba"},
+    {"480", "dance-480-gold-bar-american-mambo"},
+    {"484", "dance-484-gold-bar-ballroom-foxtrot"},
+    {"485", "dance-495-gold-bar-latin-paso-doble"},
+};
